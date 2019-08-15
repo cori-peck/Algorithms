f5a695d4ec741a6931da6944086fb9f8935befd3
cori-peck <cori.peck@ymail.com>
2019-08-15 11:50:42 -0500

    solves rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,21 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  plays = ['rock', 'paper','scissors']
+  answer = []
+
+  def helper(current_round, outcome=[]):
+    if current_round == 0:
+        answer.append(outcome)
+        return
+
+    for play in plays:
+      helper(current_round-1, outcome + [play])
+
+  helper(n, [])
+  return answer
+
+
 
 
 if __name__ == "__main__":
